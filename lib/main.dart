fc26571a817d3e2c8edf8ebbcef13afd13f9f1cf
nvshah <nipunshah6776@gmail.com>
2020-05-11 22:23:15 +0530

    Initia Commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import './categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meals App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      //Widget that points as first screen in application
+      home: CategoriesScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals App'),
+      ),
+      body: Center(
+        child: Text("Some Data"),
+      ),
+    );
+  }
+}
