725a688b834def60d94fedff0a26f0cf2fa8171a
nvshah <nipunshah6776@gmail.com>
2020-05-11 22:23:58 +0530

    Categories Screen & Categories Item

---
+++ lib/categories_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import './dummy_data.dart';
+import './category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Thy Meal'),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(15),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(catData.title, catData.color))
+            .toList(),
+        //Sliver means scrollable | according to width available of device
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          //How many max width in terms of pixel
+          maxCrossAxisExtent: 200,
+          //Ratio of height to width for each children delegate/contestant
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
