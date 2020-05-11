c6ee8cbc345ed27dcfe217c931c4d1165484878d
nvshah <nipunshah6776@gmail.com>
2020-05-12 00:04:24 +0530

    Added fonts & theming a little bit Initiate Navigation process for Category Item details page

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,27 +1,45 @@
 import 'package:flutter/material.dart';
 
+import './categories_screen.dart';
+
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color; // for background
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext ctxt) {
+    Navigator.of(ctxt).push(MaterialPageRoute(
+      builder: (_) {
+        return CategoriesScreen();
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            //adjusting transparency of color shades
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              //adjusting transparency of color shades
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Meals App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.purple,
+          accentColor: Colors.pinkAccent,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                body1: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                body2: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                title: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
       //Widget that points as first screen in application
       home: CategoriesScreen(),
     );
